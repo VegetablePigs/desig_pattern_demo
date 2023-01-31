61e687be46184f7eddc98111fe9bdf3ebde266ff
spring-cloud-demo <https://github.com/VegetablePigs/spring-cloud-demo.git>
2023-01-31 17:47:08 +0800

    创建项目

---
+++ src/test/java/com/jyd/desig_pattern_demo/DesigPatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyd.desig_pattern_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesigPatternDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
