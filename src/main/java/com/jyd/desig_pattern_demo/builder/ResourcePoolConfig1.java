e25b1c7d431eeb2d83162c0e0c5a0fec7207721a
spring-cloud-demo <https://github.com/VegetablePigs/spring-cloud-demo.git>
2023-02-03 15:50:36 +0800

    工厂模式&原型模式

---
+++ src/main/java/com/jyd/desig_pattern_demo/builder/ResourcePoolConfig1.java
@@ -0,0 +1,49 @@
+package com.jyd.desig_pattern_demo.builder;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * 资源池配置
+ *
+ * @author jyd
+ * @date 2023/02/02 17:43:28
+ */
+public class ResourcePoolConfig1 {
+  private static final int DEFAULT_MAX_TOTAL = 8;
+  private static final int DEFAULT_MAX_IDLE = 8;
+  private static final int DEFAULT_MIN_IDLE = 0;
+
+  private String name;
+  private int maxTotal = DEFAULT_MAX_TOTAL;
+  private int maxIdle = DEFAULT_MAX_IDLE;
+  private int minIdle = DEFAULT_MIN_IDLE;
+
+  public ResourcePoolConfig1(String name, Integer maxTotal, Integer maxIdle, Integer minIdle) {
+    if (StringUtils.isEmpty(name)) {
+      throw new IllegalArgumentException("name should not be empty.");
+    }
+    this.name = name;
+
+    if (maxTotal != null) {
+      if (maxTotal <= 0) {
+        throw new IllegalArgumentException("maxTotal should be positive.");
+      }
+      this.maxTotal = maxTotal;
+    }
+
+    if (maxIdle != null) {
+      if (maxIdle < 0) {
+        throw new IllegalArgumentException("maxIdle should not be negative.");
+      }
+      this.maxIdle = maxIdle;
+    }
+
+    if (minIdle != null) {
+      if (minIdle < 0) {
+        throw new IllegalArgumentException("minIdle should not be negative.");
+      }
+      this.minIdle = minIdle;
+    }
+  }
+  //...省略getter方法...
+}
